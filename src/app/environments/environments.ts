9e8bdd016f392433166797894ba0eb4e70644ae0
unknown <matheuscoronado06@gmail.com>
2025-12-04 01:49:14 -0500

    deploy.2

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // Asegúrate que este sea el puerto donde corre tu backend de Spring Boot
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'http://localhost:10000/api'
 };
